4650ffb051c98bfe452aba916284d411b7f2b56a
Dariusz Rybicki <darrarski@gmail.com>
2015-04-26 22:05:49 +0200

    DRCircularProgressView - implementation with two separate CAShapeLayer's

--- DRCircularProgress/DRCircularProgressView.m
+++ DRCircularProgress/DRCircularProgressView.m
@@ -5,7 +5,16 @@
 
 #import "DRCircularProgressView.h"
 
+@interface DRCircularProgressView ()
+
+@property (nonatomic, weak) CAShapeLayer *backgroundOvalLayer;
+@property (nonatomic, weak) CAShapeLayer *progressOvalLayer;
+
+@end
+
 @implementation DRCircularProgressView
+
+#pragma mark - Initialization
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -26,48 +35,98 @@
 - (void)commonInit
 {
     _progressValue = 0.f;
-    _progressColor = [UIColor greenColor];
-    _alternativeColor = [UIColor grayColor];
+    _progressColor = [UIColor blueColor];
+    _alternativeColor = [UIColor lightGrayColor];
     _thickness = 10.f;
+    [self progressOvalLayer];
+    [self backgroundOvalLayer];
 }
 
-- (void)drawRect:(CGRect)rect
+#pragma mark - Public properties
+
+- (void)setProgressValue:(CGFloat)progressValue
 {
-    [super drawRect:rect];
-
-    CGFloat progressAngle = -self.progressValue * 360.f + 90.f;
-
-    [self drawOvalInRect:rect
-              startAngle:-90.f
-                endAngle:-progressAngle
-               thicsness:self.thickness
-                   color:self.progressColor];
-
-    [self drawOvalInRect:rect
-              startAngle:-progressAngle
-                endAngle:270.f
-               thicsness:self.thickness
-                   color:self.alternativeColor];
+    _progressValue = progressValue;
+    self.progressOvalLayer.strokeEnd = MIN(1, MAX(0, progressValue));
+    self.backgroundOvalLayer.strokeEnd = MIN(1, MAX(0, 1 - progressValue));
 }
 
-- (void)drawOvalInRect:(CGRect)rect startAngle:(CGFloat)startAngle endAngle:(CGFloat)endAngle thicsness:(CGFloat)thickness color:(UIColor *)color
+- (void)setProgressColor:(UIColor *)progressColor
+{
+    _progressColor = progressColor;
+    self.progressOvalLayer.strokeColor = progressColor.CGColor;
+}
+
+- (void)setAlternativeColor:(UIColor *)alternativeColor
+{
+    _alternativeColor = alternativeColor;
+    self.backgroundOvalLayer.strokeColor = alternativeColor.CGColor;
+}
+
+- (void)setThickness:(CGFloat)thickness
+{
+    _thickness = thickness;
+    self.progressOvalLayer.lineWidth = thickness;
+    self.backgroundOvalLayer.lineWidth = thickness;
+}
+
+#pragma mark - Internals
+
+- (CAShapeLayer *)backgroundOvalLayer
+{
+    if (!_backgroundOvalLayer) {
+        CAShapeLayer *layer = [CAShapeLayer layer];
+        layer.frame = self.bounds;
+        layer.fillColor = nil;
+        layer.strokeColor = self.alternativeColor.CGColor;
+        layer.lineWidth = self.thickness;
+        layer.path = [[self class] ovalPathInRect:CGRectInset(self.bounds, self.thickness / 2.f, self.thickness / 2.f)
+                                       startAngle:270
+                                         endAngle:-90
+                                        clockwise:NO].CGPath;
+        layer.strokeStart = 0;
+        layer.strokeEnd = MIN(1, MAX(0, 1 - self.progressValue));
+        [self.layer addSublayer:layer];
+        _backgroundOvalLayer = layer;
+    }
+    return _backgroundOvalLayer;
+}
+
+- (CAShapeLayer *)progressOvalLayer
+{
+    if (!_progressOvalLayer) {
+        CAShapeLayer *layer = [CAShapeLayer layer];
+        layer.frame = self.bounds;
+        layer.fillColor = nil;
+        layer.strokeColor = self.progressColor.CGColor;
+        layer.lineWidth = self.thickness;
+        layer.path = [[self class] ovalPathInRect:CGRectInset(self.bounds, self.thickness / 2.f, self.thickness / 2.f)
+                                       startAngle:-90
+                                         endAngle:270
+                                        clockwise:YES].CGPath;
+        layer.strokeStart = 0;
+        layer.strokeEnd = MIN(1, MAX(0, self.progressValue));
+        [self.layer addSublayer:layer];
+        _progressOvalLayer = layer;
+    }
+    return _progressOvalLayer;
+}
+
++ (UIBezierPath *)ovalPathInRect:(CGRect)rect startAngle:(CGFloat)startAngle endAngle:(CGFloat)endAngle clockwise:(BOOL)clockwise
 {
     BOOL isCircle = rect.size.width == rect.size.height;
-    CGRect ovalRect = CGRectInset(rect, thickness / 2.f, thickness / 2.f);
     UIBezierPath *ovalPath = [UIBezierPath bezierPath];
-    [ovalPath addArcWithCenter:isCircle ? CGPointMake(CGRectGetMidX(ovalRect), CGRectGetMidY(ovalRect)) : CGPointZero
-                        radius:CGRectGetWidth(ovalRect) / 2.f
+    [ovalPath addArcWithCenter:isCircle ? CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect)) : CGPointZero
+                        radius:CGRectGetWidth(rect) / 2.f
                     startAngle:startAngle * (CGFloat) M_PI / 180.f
                       endAngle:endAngle * (CGFloat) M_PI / 180.f
-                     clockwise:YES];
+                     clockwise:clockwise];
     if (!isCircle) {
         CGAffineTransform ovalTransform = CGAffineTransformMakeTranslation(CGRectGetMidX(rect), CGRectGetMidY(rect));
-        ovalTransform = CGAffineTransformScale(ovalTransform, 1, CGRectGetHeight(ovalRect) / CGRectGetWidth(ovalRect));
+        ovalTransform = CGAffineTransformScale(ovalTransform, 1, CGRectGetHeight(rect) / CGRectGetWidth(rect));
         [ovalPath applyTransform:ovalTransform];
     }
-    [color setStroke];
-    ovalPath.lineWidth = thickness;
-    [ovalPath stroke];
+    return ovalPath;
 }
 
 @end
